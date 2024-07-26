d2f7f06b2c1ebc0bf87083fb28e512805f76d3d9
Abin Simon <mail@meain.io>
2024-07-26 21:37:28 +0530

    Basic async setup working

--- yap-utils.el
+++ yap-utils.el
@@ -4,6 +4,18 @@
 ;; Bunch of util functions for yap
 
 ;;; Code:
+(defvar yap-respond-in-buffer nil
+  "Whether to respond in a new buffer or the echo area.")
+(defvar yap-respond-in-buffer-threshold 300
+  "If the response is longer than this, always respond in a new buffer.")
+(defvar yap-show-diff-before-rewrite t
+  "Whether to show the diff before rewriting the buffer.")
+(defvar yap-popup-timeout 5
+  "The time in seconds to show the popup for.")
+(defvar yap-no-popup nil
+  "Whether to show the response in a popup or not.
+If non-nil, it will use the echo area.")
+
 (defvar yap--response-buffer "*yap-response*")
 
 (defun yap--parse-csv-line (line)
@@ -73,10 +85,10 @@
         (cdr system-message)
       nil)))
 
-(defun yap--present-response (response)
+(defun yap--present-response (response original-buffer)
   "Present the RESPONSE in a posframe or a new buffer, defaulting to the echo area.
 You can always call `yap-display-output-buffer' to view the output in
-a separate buffer."
+a separate buffer. ORIGINAL-BUFFER is the buffer we initiated the request."
   (let ((buffer (get-buffer-create yap--response-buffer)))
     (with-current-buffer buffer
       (erase-buffer)
@@ -86,7 +98,8 @@ a separate buffer."
     (if (or yap-respond-in-buffer (> (length response) yap-respond-in-buffer-threshold))
         (display-buffer buffer)
       (if (and (featurep 'posframe) (fboundp 'posframe-show) (not yap-no-popup))
-          (posframe-show " *yap-response*"
+          (with-current-buffer original-buffer
+              (posframe-show " *yap-response*"
                          :string response
                          :timeout yap-popup-timeout
                          :border-width 2
@@ -96,7 +109,7 @@ a separate buffer."
                          :left-fringe 8
                          :right-fringe 8
                          :border-color (face-attribute 'vertical-border :foreground)
-                         :position (point))
+                         :position (point)))
         (message response)))))
 
 (defun yap-display-output-buffer ()
--- yap.el
+++ yap.el
@@ -31,19 +31,8 @@
   "The API key to use with OpenAI models in the yap command.")
 (defvar yap-api-key:anthropic nil
   "The API key to use with Anthropic models in the yap command.")
-(defvar yap-respond-in-buffer nil
-  "Whether to respond in a new buffer or the echo area.")
-(defvar yap-respond-in-buffer-threshold 300
-  "If the response is longer than this, always respond in a new buffer.")
-(defvar yap-show-diff-before-rewrite t
-  "Whether to show the diff before rewriting the buffer.")
 (defvar yap-log-requests nil
   "Provide a folder to log all the requests and responses.")
-(defvar yap-popup-timeout 5
-  "The time in seconds to show the popup for.")
-(defvar yap-no-popup nil
-  "Whether to show the response in a popup or not.
-If non-nil, it will use the echo area.")
 
 (defun yap--get-models:openai ()
   "Get a list of OpenAI models available."
@@ -89,10 +78,37 @@ This is a temporary solution until we have a proper API to get models."
             (model-name (completing-read "Model: " models)))
       (setq yap-model model-name)))
 
+(defun yap--process-response (messages callback parse-response)
+  "Process the response from LLM for the given set of MESSAGES and call CALLBACK.
+Use PARSE-RESPONSE to parse the response."
+  (lambda (status)
+    (if (plist-get status :error)
+        (message "[ERROR] %s" (plist-get status :error))
+      (progn
+        (set-buffer-multibyte t)  ;; This fixes accented characters and emojis
+        (goto-char (point-min))
+        (re-search-forward "^$")
+        (let ((resp (funcall parse-response (current-buffer))))
+          (if resp
+              (progn
+                (when yap-log-requests
+                  (mkdir yap-log-requests t)
+                  ;; Save logs to disk
+                  (let ((json-data (json-encode `(("service" . ,yap-service)
+                                                  ("model" . ,yap-model)
+                                                  ("messages" . ,messages)
+                                                  ("response" . ,resp))))
+                        (json-file (format "%s/%s.json" yap-log-requests (format-time-string "%Y%m%d-%H%M%S"))))
+                    (with-temp-file json-file
+                      (insert json-data))))
+                (funcall callback resp))
+            (message "[ERROR] Unable to get response %s" (yap--get-error-message resp))))))))
+
 ;; Use `(setq url-debug 1)' to debug things
 ;; Use `(setq url-debug nil)' to disable debugging
-(defun yap--get-llm-response:openai (messages)
-  "Get the response from OpenAI LLM for the given set of MESSAGES."
+(defun yap--get-llm-response:openai (messages callback)
+  "Get the response from OpenAI LLM for the given set of MESSAGES.
+Once the response is fetched call the CALLBACK."
   (let* ((inhibit-message t)
          (url-request-method "POST")
          (url-request-extra-headers
@@ -103,24 +119,25 @@ This is a temporary solution until we have a proper API to get models."
                        ("messages" . ,(yap--convert-messages messages)))))
          ;; https://github.com/pythonic-emacs/anaconda-mode/issues/189
          (url-request-data (encode-coding-string json-data 'us-ascii)) ;; TODO: shouldn't this be utf-8
-         (url-request-data-type 'json)
-         (resp (with-current-buffer (url-retrieve-synchronously
-                                     "https://api.openai.com/v1/chat/completions")
-                 (set-buffer-multibyte t)   ;; This fixes accented characters and emojis
-                 (goto-char (point-min))
-                 (re-search-forward "^$")
-                 (json-read))))
-    (if (alist-get 'choices resp)
-        (alist-get 'content
-                   (alist-get 'message
-                              (aref (alist-get 'choices resp) 0)))
-      (progn
-        (message "[ERROR] Unable to get response %s" (yap--get-error-message resp))
-        nil))))
+         (url-request-data-type 'json))
+    (url-retrieve
+     "https://api.openai.com/v1/chat/completions"
+     (yap--process-response
+      messages callback
+      (lambda (buffer)
+        (with-current-buffer buffer
+          (let* ((resp (json-read))
+                 (choices (alist-get 'choices resp)))
+            (if choices
+                (let* ((message (alist-get 'message (aref choices 0)))
+                       (content (alist-get 'content message)))
+                  content)
+              nil))))))))
 
 ;; TODO: Not tested
-(defun yap--get-llm-response:anthropic (messages)
-  "Get the response from Anthropic LLM for the given set of MESSAGES."
+(defun yap--get-llm-response:anthropic (messages callback)
+  "Get the response from Anthropic LLM for the given set of MESSAGES.
+Once the response is fetched call the CALLBACK."
   (let* ((inhibit-message t)
          (url-request-method "POST")
          (url-request-extra-headers
@@ -132,39 +149,25 @@ This is a temporary solution until we have a proper API to get models."
              ("system" . ,(yap--system-message messages))
              ("messages" . ,(yap--convert-messages-sans-system messages)))))
          (url-request-data (encode-coding-string json-data 'us-ascii))
-         (url-request-data-type 'json)
-         (resp (with-current-buffer (url-retrieve-synchronously
-                                     "https://api.anthropic.com/v1/messages")
-                 (set-buffer-multibyte t)   ;; This fixes accented characters and emojis
-                 (goto-char (point-min))
-                 (re-search-forward "^$")
-                 (json-read))))
-    (if (alist-get 'content resp)
-        (alist-get 'text (aref (alist-get 'content resp) 0))
-      (progn
-        (message "[ERROR] Unable to get response %s" (yap--get-error-message resp))
-        nil))))
+         (url-request-data-type 'json))
+    (url-retrieve "https://api.anthropic.com/v1/messages"
+                  (yap--process-response
+                   messages callback
+                   (lambda (buffer)
+                     (with-current-buffer buffer
+                       (let* ((resp (json-read))
+                              (content (alist-get 'content resp)))
+                         (if content
+                             (alist-get 'text (aref content 0))
+                           nil))))))))
 
-(defun yap--get-llm-response (messages)
-  "Get the response from LLM for the given set of MESSAGES."
-  (progn
-    (message "Processing request via %s and %s model..." yap-service yap-model)
-    (let ((response (pcase yap-service
-                      ("openai" (yap--get-llm-response:openai messages))
-                      ("anthropic" (yap--get-llm-response:anthropic messages))
-                      (_ (message "[ERROR] Unsupported service: %s" yap-service) nil))))
-      (progn
-        (when (and response yap-log-requests)
-          (mkdir yap-log-requests t)
-          ;; Save logs to disk
-          (let ((json-data (json-encode `(("service" . ,yap-service)
-                                          ("model" . ,yap-model)
-                                          ("messages" . ,messages)
-                                          ("response" . ,response))))
-                (json-file (format "%s/%s.json" yap-log-requests (format-time-string "%Y%m%d-%H%M%S"))))
-            (with-temp-file json-file
-              (insert json-data))))
-        response))))
+(defun yap--get-llm-response (messages callback)
+  "Get the response from LLM for the given set of MESSAGES and call the CALLBACK."
+  (message "Processing request via %s and %s model..." yap-service yap-model)
+  (pcase yap-service
+    ("openai" (yap--get-llm-response:openai messages callback))
+    ("anthropic" (yap--get-llm-response:anthropic messages callback))
+    (_ (message "[ERROR] Unsupported service: %s" yap-service) nil)))
 
 (defun yap-prompt (&optional template)
   "Prompt the user with the given PROMPT using TEMPLATE if provided.
@@ -177,10 +180,10 @@ The response from LLM is displayed in the *yap-response* buffer."
                      (or template 'default-prompt))) ; Otherwise, use default template if not provided
          (llm-messages (yap--get-filled-template template)))
     (if llm-messages
-        (let ((response (yap--get-llm-response llm-messages)))
-          (if response
-              (yap--present-response response)
-            (message "[ERROR] Failed to get a response from LLM")))
+        ;; TODO yap--get-llm-response is not a good name anymore
+        (let ((buffer (current-buffer)))
+          (yap--get-llm-response llm-messages (lambda (response)
+                                                (funcall #'yap--present-response response buffer))))
       (message "[ERROR] Failed to fill template"))))
 
 (defun yap-rewrite (&optional template)
@@ -192,7 +195,10 @@ Rewrite the buffer or selection if present with the returned response."
                      (or template 'default-rewrite))) ; Otherwise, use default template if not provided
          (llm-messages (yap--get-filled-template template)))
     (if llm-messages
-        (yap--rewrite-buffer-or-selection (yap--get-llm-response llm-messages) (current-buffer))
+        (let ((buffer (current-buffer)))
+          (yap--get-llm-response llm-messages
+                                 (lambda (message)
+                                   (yap--rewrite-buffer-or-selection message buffer))))
       (message "[ERROR] Failed to fill template"))))
 
 (defun yap-write (&optional template)
@@ -204,7 +210,11 @@ Kinda like `yap-rewrite', but just writes instead of replace."
                      (or template 'default-rewrite))) ; Otherwise, use default template if not provided
          (llm-messages (yap--get-filled-template template)))
     (if llm-messages
-        (insert (yap--get-llm-response llm-messages))
+        (let ((buffer (current-buffer)))
+          (yap--get-llm-response llm-messages
+                                 (lambda (message)
+                                   (with-current-buffer buffer
+                                     (insert message)))))
       (message "[ERROR] Failed to fill template"))))
 
 (provide 'yap)
