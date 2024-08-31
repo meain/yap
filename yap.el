8582f20af8dafb9fee7c5262d6a26d4b0c2a3a47
Abin Simon <mail@meain.io>
2024-08-31 15:53:27 +0530

    Add support for streaming response

--- yap.el
+++ yap.el
@@ -3,7 +3,7 @@
 ;; URL: https://github.com/meain/yap
 ;; Keywords: llm, convenience
 ;; SPDX-License-Identifier: Apache-2.0
-;; Package-Requires: ((emacs "25.1"))
+;; Package-Requires: ((emacs "25.1") (plz "0.9"))
 ;; Version: 0.1
 
 ;;; Commentary:
@@ -19,6 +19,7 @@
 
 (require 'json)
 (require 'url)
+(require 'plz)
 
 (require 'yap-templates)
 (require 'yap-utils)
@@ -81,67 +82,104 @@
                      ("openai" "gpt-4o"))))
       (setq yap-model model)))
 
-(defun yap--process-response (messages callback parse-response)
-  "Process the response from LLM for the given set of MESSAGES and call CALLBACK.
-Use PARSE-RESPONSE to parse the response."
-  (lambda (status)
-    (if (plist-get status :error)
-        (message "[ERROR] %s" (plist-get status :error))
+(defun yap--process-response (messages resp callback)
+  "Process the final complete response from LLM.
+MESSAGES is the original messages sent to LLM.
+RESP is the final response from LLM.
+CALLBACK is the function to call with the final response."
+  (if resp
       (progn
-        (set-buffer-multibyte t)  ;; This fixes accented characters and emojis
-        (goto-char (point-min))
-        (re-search-forward "^$")
-        (let ((resp (funcall parse-response (current-buffer))))
-          (if resp
-              (progn
-                (when yap-log-requests
-                  (mkdir yap-log-requests t)
-                  ;; Save logs to disk
-                  (let ((json-data (json-encode `(("service" . ,yap-service)
-                                                  ("model" . ,yap-model)
-                                                  ("messages" . ,messages)
-                                                  ("response" . ,resp))))
-                        (json-file (format "%s/%s.json" yap-log-requests (format-time-string "%Y%m%d-%H%M%S"))))
-                    (with-temp-file json-file
-                      (insert json-data))))
-                (funcall callback resp))
-            (message "[ERROR] Unable to get response %s" (yap--get-error-message resp))))))))
+        (when yap-log-requests
+          (mkdir yap-log-requests t)
+          ;; Save logs to disk
+          (let ((json-data (json-encode `(("service" . ,yap-service)
+                                          ("model" . ,yap-model)
+                                          ("messages" . ,messages)
+                                          ("response" . ,resp))))
+                (json-file (format "%s/%s.json" yap-log-requests (format-time-string "%Y%m%d-%H%M%S"))))
+            (with-temp-file json-file
+              (insert json-data))))
+        (if callback (funcall callback resp)))
+    (message "[ERROR] Unable to get response %s" (yap--get-error-message resp))))
 
 ;; Use `(setq url-debug 1)' to debug things
 ;; Use `(setq url-debug nil)' to disable debugging
-(defun yap--get-llm-response:openai (messages callback)
+(defun yap--get-llm-response:openai (messages partial-callback &optional final-callback)
   "Get the response from OpenAI LLM for the given set of MESSAGES.
-Once the response is fetched call the CALLBACK."
+PARTIAL-CALLBACK is called with each chunk of the response.
+FINAL-CALLBACK is called with the final response."
   (let* ((inhibit-message t)
-         (url-request-method "POST")
-         (url-request-extra-headers
+         (headers
           `(("Content-Type" . "application/json")
             ("Authorization" . ,(format "Bearer %s" yap-api-key:openai))))
          (json-data (json-encode
                      `(("model" . ,yap-model)
-                       ("messages" . ,(yap--convert-messages messages)))))
-         ;; https://github.com/pythonic-emacs/anaconda-mode/issues/189
-         (url-request-data (encode-coding-string json-data 'us-ascii)) ;; TODO: shouldn't this be utf-8
-         (url-request-data-type 'json))
-    (url-retrieve
-     "https://api.openai.com/v1/chat/completions"
-     (yap--process-response
-      messages callback
-      (lambda (buffer)
-        (with-current-buffer buffer
-          (let* ((resp (json-read))
-                 (choices (alist-get 'choices resp)))
-            (if choices
-                (let* ((message (alist-get 'message (aref choices 0)))
-                       (content (alist-get 'content message)))
-                  content)
-              nil))))))))
+                       ("stream" . t)
+                       ("messages" . ,(yap--convert-messages messages))))))
+    (let ((prev-pending "")
+          (full-message "")
+          (initial-message t)
+          (inhibit-message t))
+      (plz 'post
+        "https://api.openai.com/v1/chat/completions"
+        :headers headers
+        :body json-data
+        :as 'string
+        :then (lambda (_)
+                (when final-callback
+                  (funcall final-callback
+                           (with-current-buffer yap--response-buffer (buffer-string)))))
+        :filter (lambda (process output)
+                  ;; Let plz do its thing
+                  ;; This code is from `test-plz-process-filter'
+                  (when (buffer-live-p (process-buffer process))
+                    (with-current-buffer (process-buffer process)
+                      (let ((movingp (= (point) (process-mark process))))
+                        (save-excursion
+                          (goto-char (process-mark process))
+                          (insert output)
+                          (set-marker (process-mark process) (point)))
+                        (when movingp
+                          (goto-char (process-mark process))))))
 
-(defun yap--get-llm-response (messages callback)
-  "Get the response from LLM for the given set of MESSAGES and call the CALLBACK."
+                  (let* ((filtered-output
+                          (if initial-message
+                              (progn
+                                (setq initial-message nil)
+                                (substring output (string-match "\r\n\r\n" output))) ; trim out headers
+                            (concat prev-pending output)))
+                         (lines (split-string filtered-output "\n" t))
+                         ;; trim out empty lines including ones with just \r
+                         (non-empty-lines (seq-filter (lambda (x) (length> x 2)) lines))
+                         ;; trim out `data: ` prefix from each line
+                         (trimmed-lines (mapcar (lambda (x) (substring x 6)) non-empty-lines))
+                         ;; filter out lines that are not json as line can but cut off half way through
+                         (json-lines (seq-filter (lambda (x) (ignore-errors (json-read-from-string x))) trimmed-lines))
+                         ;; convert to json-objects
+                         (json-objects (mapcar 'json-read-from-string json-lines)))
+                    (with-current-buffer yap--response-buffer
+                      ;; Save last line to prev-pending in case it is cut off
+                      (if (< (length json-lines) (length trimmed-lines))
+                          (setq prev-pending (car (last lines)))
+                        (setq prev-pending ""))
+
+                      ;; Data is in choices.0.delta.content
+                      (mapc (lambda (x)
+                              (let ((message
+                                     (alist-get 'content
+                                                (alist-get 'delta
+                                                           (aref (alist-get 'choices x) 0)))))
+                                (when (and partial-callback message)
+                                  (funcall partial-callback message))))
+                            json-objects))))))))
+
+(defun yap--get-llm-response (messages partial-callback &optional final-callback)
+  "Get the response from LLM for the given set of MESSAGES.
+PARTIAL-CALLBACK is called with each chunk of the response.
+FINAL-CALLBACK is called with the final response."
   (message "Processing request via %s and %s model..." yap-service yap-model)
   (pcase yap-service
-    ("openai" (yap--get-llm-response:openai messages callback))
+    ("openai" (yap--get-llm-response:openai messages partial-callback final-callback))
     (_ (message "[ERROR] Unsupported service: %s" yap-service) nil)))
 
 (defun yap-prompt (&optional template)
@@ -157,8 +195,11 @@ The response from LLM is displayed in the *yap-response* buffer."
     (if llm-messages
         ;; TODO yap--get-llm-response is not a good name anymore
         (let ((buffer (current-buffer)))
-          (yap--get-llm-response llm-messages (lambda (response)
-                                                (funcall #'yap--present-response response buffer))))
+          (yap--clean-response-buffer)
+          (yap-show-response-buffer)
+          (yap--get-llm-response llm-messages
+                                 (lambda (chunk)
+                                   (yap--insert-chunk-to-response-buffer chunk))))
       (message "[ERROR] Failed to fill template"))))
 
 (defun yap-rewrite (&optional template)
@@ -173,8 +214,13 @@ Rewrite the buffer or selection if present with the returned response."
         (let ((buffer (current-buffer))
               (start (if (region-active-p) (region-beginning) (point-min)))
               (end (if (region-active-p) (region-end) (point-max))))
+          (yap--clean-response-buffer)
+          (yap-show-response-buffer)
           (yap--get-llm-response llm-messages
+                                 (lambda (chunk)
+                                   (yap--insert-chunk-to-response-buffer chunk))
                                  (lambda (message)
+                                   (yap--hide-response-buffer)
                                    (yap--rewrite-buffer-or-selection message buffer start end))))
       (message "[ERROR] Failed to fill template"))))
 
